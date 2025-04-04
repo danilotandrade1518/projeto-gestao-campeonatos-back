b32693949b7f0be768b76a0309a1c01587c417c6
Danilo Andrade <danilotandrade1518@gmail.com>
2025-04-04 08:51:14 -0300

    chore: update MongoDB database initialization to use default database name

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -2,4 +2,4 @@ import 'dotenv/config';
 import { MongoClient } from 'mongodb';
 
 export const client = new MongoClient(process.env.MONGO_URI as string);
-export const db = client.db('championship');
+export const db = client.db();
